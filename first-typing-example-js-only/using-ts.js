93ff9397894fb6442fcf3b2d6847d436ae156e15
Faddah Wolf <my_biz@me.com>
2024-04-07 12:23:37 -0700

    Final version, using-ts.js

--- first-typing-example-js-only/using-ts.js
+++ first-typing-example-js-only/using-ts.js
@@ -1,10 +1,11 @@
-console.log("using-ts.js in da hizzzzzzouse!!!");
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-var result = document.getElementById("result");
-var add = function (num1, num2) { return num1 + num2; };
-button.addEventListener("click", function () {
+"use strict";
+console.log(`using-ts.js in da hizzzzzzouse!!!`);
+const button = document.querySelector(`button`);
+const input1 = document.getElementById(`num1`);
+const input2 = document.getElementById(`num2`);
+const result = document.getElementById(`result`);
+const add = (num1, num2) => num1 + num2;
+button.addEventListener(`click`, () => {
     console.log(add(+input1.value, +input2.value));
     result.textContent = add(+input1.value, +input2.value).toString();
 });
