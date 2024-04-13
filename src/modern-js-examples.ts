766341aa654fe19c5697e904268009aebf0b8f48
Faddah Wolf <my_biz@me.com>
2024-04-13 14:08:23 -0700

    Changed name to modern-js-example.ts.

---
+++ src/modern-js-examples.ts
@@ -0,0 +1,80 @@
+const theName: string = 'Faddah';
+// theName = 'Max'; // this will throw an error because theName is a const
+console.log(theName);
+
+let age: number = 63;
+
+age = 29;
+// var theResult;
+
+function addUp(a: number, b: number) {
+	let theResult;
+	theResult = a + b;
+	return theResult;
+}
+
+// console.log(theResult);
+
+// let anAge = 30
+
+// if (anAge > 20) {
+// 	let isOld = true;
+// }
+
+// console.log(isOld);
+
+const adding = (a: number, b: number = 7.3): number => a + b;
+
+console.log(adding(2, 5));
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+printOutput(adding(5, 2));
+
+printOutput(adding(20));
+
+printOutput('hey baba re bop, faddah!');
+
+const myButton: HTMLButtonElement = document.querySelector('button')!;
+
+if (myButton) {
+	myButton.addEventListener('click', event => console.log(event));
+}
+
+const hobbies: string[] = ['Sports', 'Cooking'];
+const activeHobbies: string[] = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+interface Person {
+	firstName: string;
+	age: number;
+}
+
+const aPerson: Person = {
+	firstName: 'Faddah',
+	age: 63
+};
+
+const copiedPerson: Person = { ...aPerson };
+
+console.log(copiedPerson);
+
+// using spread operator for params in functions
+const addAll = (...numbers: number[]): number => numbers.reduce((curResult: number, curValue: number): number => curResult + curValue, 0);
+
+const addedNumbers = addAll(5, 10, 2, 3.7);
+
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: aName, age: theAge } = aPerson;
+
+console.log(aName, theAge, aPerson);
+
+
+
+
