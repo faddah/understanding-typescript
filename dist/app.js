cd896edf1d51f3107b56a16bcac5da1539cc4995
Faddah Wolf <my_biz@me.com>
2024-04-13 20:00:14 -0700

    Generated by tsc.

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,28 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
-        console.log('Department: ' + this.name);
+        console.log(`Department:\t${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(`No. of Employees: ${this.employees.length}`);
+        console.table(this.employees);
     }
 }
 const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Faddah');
+accounting.addEmployee('Darcy');
+accounting.addEmployee('Iris');
+accounting.addEmployee('Casey');
+accounting.employees[2] = 'Allison';
 console.log(accounting);
+accounting.describe();
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
