39a03b9e8ba9398e36b3e0479908028290ec82f9
Faddah Wolf <my_biz@me.com>
2024-05-06 15:13:57 -0700

    Generated by TSC.

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,12 @@
+var _a;
 console.log("it's advanced TS feature time, bay-bee!");
 import { buttonClick } from "./button.js";
 import test from "./test.js";
 buttonClick();
 test();
+var paragraph = document.querySelector('p');
+var paraById = document.getElementById('message-output');
+var userInputElement = document.getElementById('user-input');
 var e1 = {
     name: "Faddah",
     privileges: ["create-server"],
@@ -19,6 +23,19 @@ var uni;
 function add(a, b) {
     return (typeof a === "string" || typeof b === "string") ? a.toString() + b.toString() : a + b;
 }
+var result = add(5, 3);
+console.log(result);
+var result2 = add('Faddah', ' Wolf');
+console.log(result2.split(' '));
+var fetchedUserData = {
+    id: "u1",
+    name: "Faddah",
+    job: { title: "CEO", description: "EatMeBumCo" }
+};
+console.log("Job Title: ".concat((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title));
+var theInput = '';
+var storedData = theInput !== null && theInput !== void 0 ? theInput : 'DEFAULT';
+console.log("The Input: ".concat(storedData));
 function printEmployeeInformation(emp) {
     console.log("Name: ".concat(emp.name));
     if ("privileges" in emp)
@@ -48,7 +65,7 @@ var Truck = (function () {
         console.log("Driving a truck...");
     };
     Truck.prototype.loadCargo = function (amount) {
-        console.log("Loading cargo... ".concat(amount));
+        console.log("Loading cargo...".concat(amount));
     };
     return Truck;
 }());
@@ -74,4 +91,16 @@ function moveAnimal(animal) {
     console.log("The ".concat(animal.type, " is moving at a speed of ").concat(speed, " kph."));
 }
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+if (userInputElement)
+    userInputElement.value = "Hi there!";
+var outputInput = function () {
+    userInputElement.addEventListener('keyup', function (event) {
+        userInputElement.value ? paragraph.textContent = userInputElement.value : paragraph.textContent = "";
+    });
+};
+outputInput();
+var errorBag = {
+    email: "Not a valid email!",
+    username: "Must start with a capital character!",
+};
 //# sourceMappingURL=app.js.map
