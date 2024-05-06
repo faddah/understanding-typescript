3659c44227701b474951ad4f4c171a81ace4ca1a
Faddah Wolf <my_biz@me.com>
2024-05-06 15:11:52 -0700

    Added examples of TS TypeCasting, Index Properties, Function Overloads, Optional Chaining, & Nullish Coalescing.

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,14 @@ import test from "./test.js";
 buttonClick();
 test();
 
+const paragraph: HTMLParagraphElement | null = document.querySelector('p')!;
+const paraById: HTMLElement | null = document.getElementById('message-output')!;
+// TypeCasting — what's between the `<>` is forcing a TypeCasting
+// Version 1
+// const userInputElement: HTMLInputElement | null = <HTMLInputElement>document.getElementById('user-input')!;
+// Version 2 - to not conflict with React.JS `<>` build tools
+const userInputElement: HTMLInputElement | null = document.getElementById('user-input')! as HTMLInputElement;
+
 type Admin = {
 	name: string;
 	privileges: string[];
@@ -59,9 +67,36 @@ let uni: Universal;
 
 // Type Guards
 
-function add(a: Combinable, b: Combinable) {
+// Function OVerloads (overloading args)
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: number, b: string): string;
+function add(a: string, b: number): string;
+function add(a: Combinable, b: Combinable): Combinable {
 	return (typeof a === "string" || typeof b === "string") ? a.toString() + b.toString() : a + b;
 }
+
+const result = add(5, 3);
+console.log(result);
+const result2 = add('Faddah', ' Wolf'); // <= now this works with the split and without having to add `as string`, due to function overloads above 
+console.log(result2.split(' '));
+
+// Optional Chaining
+const fetchedUserData = {
+	id: "u1",
+	name: "Faddah",
+	job: { title: "CEO", description: "EatMeBumCo" }
+};
+
+console.log(`Job Title: ${fetchedUserData?.job?.title}`); // <= for use when you don't know if all data has been fetched back properly
+
+// Nullish Coalescing — for when you just don't know if it's valid data coming back or not
+// const theInput = null;
+const theInput = '';
+const storedData = theInput ?? 'DEFAULT' // In TypeScript, ?? is the Nullish Coalescing Operator, now will do `DEFAULT` only with NULL or Undefined, not other falsey vars
+console.log(`The Input: ${storedData}`);
+
+
 
 type UnknownEmployee = Employee | Admin;
 
@@ -80,6 +115,7 @@ const e3: UnknownEmployee = {
 printEmployeeInformation(e1);
 printEmployeeInformation(e3);
 
+// Discriminated Unions Examples
 class Car {
 	drive() {
 		console.log("Driving...");
@@ -92,7 +128,7 @@ class Truck {
 	}
 
 	loadCargo(amount: number) {
-		console.log(`Loading cargo... ${amount}`);
+		console.log(`Loading cargo...${amount}`);
 	}
 }
 
@@ -135,3 +171,31 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: 'bird', flyingSpeed: 10 });
+
+// Type Casting - TypeScript no likey generic `HTMLElement` type, 
+// 'cuz generic HTMLElement type doesn't know it has a `value`.
+// So, we need to force TypeScript to do it.
+// now employed with a truthy check
+
+if (userInputElement) userInputElement.value = "Hi there!";
+
+const outputInput = () => {
+	userInputElement.addEventListener('keyup', (event: KeyboardEvent) => {
+		// event.preventDefault();
+		userInputElement.value ? paragraph.textContent = userInputElement.value : paragraph.textContent = ""
+	});
+};
+outputInput();
+
+// Index Properties and Types
+interface ErrorContainer {
+	[prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+	email: "Not a valid email!",
+	username: "Must start with a capital character!",
+};
+
+
+
