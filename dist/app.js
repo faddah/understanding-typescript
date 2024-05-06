5befedcf1889ec2ea8189c99fa13f5fda95bd2b0
Faddah Wolf <my_biz@me.com>
2024-05-06 10:43:48 -0700

    Generated by tsc.

--- dist/app.js
+++ dist/app.js
@@ -61,4 +61,17 @@ function useVehicle(vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    var speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+            break;
+    }
+    console.log("The ".concat(animal.type, " is moving at a speed of ").concat(speed, " kph."));
+}
+moveAnimal({ type: 'bird', flyingSpeed: 10 });
 //# sourceMappingURL=app.js.map
