4da92c8c3d4279c21ca8f7080c063a85536db173
Faddah Wolf <my_biz@me.com>
2024-05-19 08:38:50 -0700

    Finished all current examples of Generics in TS.

--- src/app.ts
+++ src/app.ts
@@ -49,16 +49,119 @@ console.log(merge({ name: "Faddah" }, { age: 63 }));
 const mergedObj = merge({ name: "Faddah" }, { age: 63 }); // Can fix by adding `as { name: string, age: number }`, but that is cumbersome
 // console.log(mergedObj.name);  // <= TS will complain if you try to access as it just knows it's an Object, but not of the keys/properties inside it.
 
-function merge2<T, U>(objA: T extends Object, objB: U) {  // the `extends Object` part happened in later version of TS
-	return Object.assign(objA, objB);
+function merge2<T extends object | null, U extends object>(objA: T, objB: U) {  // the `extends Object` part happened in later version of TS
+	return Object.assign({}, objA, objB);
 }
 
-const mergedObj2 = merge2({ name: "Faddah" }, { age: 63 });
+const mergedObj2 = merge2({ name: "Faddah", hobbies: ['Star Trek Fan'] }, { age: 63 });
 // Can also Specify/Assign the Types of the Objects at runtime for Generics
 const mergedObj3 = merge2<{ name: string, hobbies: string[] }, { age: number }>({ name: "Faddah", hobbies: ['Bread Baking'] }, { age: 63 });
+console.log(mergedObj2);
+console.log(mergedObj3);
+
 console.log(mergedObj2.age);
 console.log(mergedObj3.hobbies);
 
+const mergedObj4 = merge2(mergedObj2, mergedObj3);
+console.log(mergedObj4);
 
+interface Lengthy {
+	length: number;
+}
 
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {	// <= this is a Generic Type Constraint
+	let descriptionText = 'Got no value.';
+	switch (element.length) {
+		case 0:
+			descriptionText = 'Got no value.';
+			break;
+		case 1:
+			descriptionText = 'Got 1 element.';
+			break;
+		default:
+			descriptionText = 'Got ' + element.length + ' elements.';
+	}
+	return [element, descriptionText];
+}
 
+console.log(countAndDescribe('Hi there!'));
+console.log(countAndDescribe(''));
+console.log(countAndDescribe(['Cooking', 'Baking', 'Star Trek']));
+
+function etl<T extends object, U extends keyof T>(obj: T, key: U) {
+	return `Value:\t${obj[key]}`;
+}
+console.log(etl({ name: 'Schlemmie' }, 'name'));
+
+class DataStorage<T extends string | number | boolean> {   // <= this is a Generic Type Constraint, you declare the type of data you want to store at the instantiation of the class and then it will be locked as that used throughout the class
+	private data: T[] = [];
+
+	addItem(item: T) {
+		this.data.push(item);
+	}
+
+	removeItem(item: T) {
+		if (this.data.indexOf(item) === -1) {
+			return;
+		}
+		this.data.splice(this.data.indexOf(item), 1);
+	}
+
+	getItems() {
+		return [...this.data];
+	}
+}
+
+const textStorage = new DataStorage<string>();
+// textStorage.addItem(true); // <= TS will complain as we've made this iteration of DataStorage a string only
+textStorage.addItem('Faddah');
+textStorage.addItem('Max');
+console.log(textStorage.getItems());
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(10);
+numberStorage.addItem(100);
+numberStorage.addItem(1000);
+console.log(numberStorage.getItems());
+numberStorage.removeItem(100);
+console.log(numberStorage.getItems());
+
+/* 
+const objStorage = new DataStorage<object>();
+const maxObj = { name: 'Max' };
+objStorage.addItem({ name: 'Faddah' });
+objStorage.addItem(maxObj);
+console.log(objStorage.getItems());
+objStorage.removeItem(maxObj);
+console.log(objStorage.getItems());
+ */
+
+// Built-in Generic Utility Types
+
+// Partial type
+interface CourseGoal {
+	title: string;
+	description: string;
+	completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+	let courseGoal: Partial<CourseGoal> = {}; // <= Partial is a Generic Utility Type, it makes all properties optional (as in `?`	optional)
+	courseGoal.title = title;
+	courseGoal.description = description;
+	courseGoal.completeUntil = date;
+	return courseGoal as CourseGoal;
+}
+
+const myGoal: CourseGoal = createCourseGoal('Learn TypeScript', 'Learn TypeScript and be a better developer', new Date(5 / 31 / 2024));
+console.log(myGoal);
+
+// Readonly type
+const namesArr: Readonly<string[]> = ['Faddah', 'Roz', 'Max'];
+// namesArr.push('Momma'); // <= TS will complain as it's a Readonly type
+// namesArr.pop(); // <= TS will complain as it's a Readonly type
+
+// Generic Types vs Union Types
+// Generics are used when you want to be flexible with the types you're working with, while Union Types are used when you want to be specific about the types you're working with.
